3a9796b5ec29defea5d0033a031492d8efc05359
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-06-02 12:10:04 +0800

    已更新

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -17,11 +17,12 @@ cron "10 0,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby
 
 let isNotify = true;//是否通知,仅限nodejs
 let ckPath = './jddj_cookie.js';//ck路径,环境变量:JDDJ_CKPATH
+let shareCodes = '';//助力码,环境变量名:JDDJ_SHARECODES,示例:code1,code2
 
 const $ = new API("jddj_fruit");
 try {
     //https://cdn.jsdelivr.net/gh/passerby-b/JDDJ@main/jddj_fruit_code.js
-    $.http.get({ url: 'https://ghproxy.com/https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit_code.js' }).then(response => {
+    $.http.get({ url: 'https://raw.githubusercontents.com/passerby-b/JDDJ/main/jddj_fruit_code.js' }).then(response => {
         if (!!response.body) eval(response.body);
     });
 } catch (error) {
